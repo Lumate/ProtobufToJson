91999f3be6dbe3c6ca379a86d38e137b5f9bfc2e
Chase Holland <lurch09@gmail.com>
2015-05-12 15:07:39 -0500

    fixing header guards (Actually) and adding missing semi-colons in header

--- protobuftojson.h
+++ protobuftojson.h
@@ -5,16 +5,16 @@
 //
 
 #ifndef PROTOBUFTOSJON
-#def PROTOBUFTOSJON
+#define PROTOBUFTOSJON
 
 //
 /// \brief write json string to provided output string
 //
-void serializeToString(google::protobuf::message::Message object, std::map<std::string, std::string> FIELDS, std::string &output)
+void serializeToString(google::protobuf::message::Message object, std::map<std::string, std::string> FIELDS, std::string &output);
 
 //
 /// \brief return json string of protobuf
 //
-std::string serializeAsString(google::protobuf::message::Message object, std::map<std::string, std::string> FIELDS)
+std::string serializeAsString(google::protobuf::message::Message object, std::map<std::string, std::string> FIELDS);
 
-#endif // PROTOBUFTOSJON
+#endif // PROTOBUFTOSJON\
