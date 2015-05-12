2f5d548d98df67dee9d145a4106944c38d547cd7
Chase Holland <lurch09@gmail.com>
2015-05-12 15:05:02 -0500

    adding header guards and fixing cmakelists

--- protobuftojson.h
+++ protobuftojson.h
@@ -4,8 +4,17 @@
 /// \brief Functions for serializing protobufs to json strings
 //
 
-// write json string to provided output string
+#ifndef PROTOBUFTOSJON
+#def PROTOBUFTOSJON
+
+//
+/// \brief write json string to provided output string
+//
 void serializeToString(google::protobuf::message::Message object, std::map<std::string, std::string> FIELDS, std::string &output)
 
-// return json string of protobuf
+//
+/// \brief return json string of protobuf
+//
 std::string serializeAsString(google::protobuf::message::Message object, std::map<std::string, std::string> FIELDS)
+
+#endif // PROTOBUFTOSJON
